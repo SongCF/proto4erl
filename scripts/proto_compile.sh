8dab889af09cc10726ca2ad6d69a9ec9a6548432
songcf <songchangfu@pxsj.com>
2016-10-11 15:45:46 +0800

    a

--- scripts/proto_compile.sh
+++ scripts/proto_compile.sh
@@ -1,3 +1,14 @@
+
+# 判断是否传入node-name
+if [ ! -n "$1" ] ;then
+    echo "======================================"
+    echo "you makefile have not input proj-name ~"
+    echo "======================================"
+    PROJ_NAME=ProjName
+else
+    PROJ_NAME=$1
+fi
+
 # 编译protobuffs协议.
 EBIN_PATH="ebin/ ../*/ebin/"
 
@@ -6,7 +17,7 @@ mkdir ../../src/proto
 erl -noshell -pa ${EBIN_PATH} \
 	-eval 'proto_tool:compile("../../proto/payload.proto", "../../src/proto/", "../../src/proto/").' \
 	-eval 'proto_tool:compile("../../proto/packet.proto", "../../src/proto/", "../../src/proto/").' \
-    -eval 'proto_tool:check("Scene", "../../proto/", "../../src/proto/").' \
+    -eval "proto_tool:check(\"${PROJ_NAME}\", \"../../proto/\", \"../../src/proto/\")." \
     -eval 'init:stop().'
 
 
